a62e3da64d9fbbab557ac162fcee43b07f26e83a
Shraddha Yeolekar <syeolekar@oanda.com>
2024-11-23 00:21:04 -0500

    fixed as per autograder

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,12 +30,12 @@ ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
  mkdir -p data/processed/server_logs data/processed/user_logs data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
- cp data/raw/rawdata/rawdata/server*.log data/processed/server_logs/
+ cp data/raw/rawdata/server*.log data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
-cp data/raw/rawdata/rawdata/user*.log data/processed/user_logs/
-cp data/raw/rawdata/rawdata/event*.log data/processed/event_logs/
+cp data/raw/rawdata/user*.log data/processed/user_logs/
+cp data/raw/rawdata/event*.log data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm data/raw/rawdata/rawdata/*ipaddr*
+rm data/raw/rawdata/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls data/processed/*/ >> data/inventory.txt
